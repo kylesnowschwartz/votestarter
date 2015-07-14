02fad3035c33626ca43e26683e35094e27c86b8a
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-14 19:46:08 +1200

    unclear about routing and restfulness

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :users
+  root 'welcome#index'
+  resources :users, except: [:index, :show] do
+    resources :pledges, only: [:new, :create]
+  end
+
   resources :candidates
+  resources :pledges, only: [:edit, :update, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
